d28ecbbb9db746af12186d3ae441c4bfe2cab0b5
leviyu <yokunu_yjy@yahoo.co.jp>
2022-03-20 16:07:55 +0900

    add docker compose file

--- server/src/redisCache/redisCache.module.ts
+++ server/src/redisCache/redisCache.module.ts
@@ -1,14 +1,24 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as redisStore from 'cache-manager-ioredis';
-import { Module, CacheModule } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisCacheService } from './redisCache.service';
 
 @Module({
   imports: [
-    CacheModule.register({
-      store: redisStore,
-      host: 'localhost',
-      port: 6379,
+    // CacheModule.register({
+    //   store: redisStore,
+    //   host: 'localhost',
+    //   port: 6379,
+    // }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_SERVER'),
+        port: configService.get('REDIS_SERVER_PORT'),
+        password: configService.get('REDIS_SERVER_PASSWORD'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [RedisCacheService],
